bebc7eef5cddcf494e953b1654a9d96e7172f6df
rishabh verma <vermarishabh0987@gmail.com>
2020-07-09 13:33:38 +0530

    basic v1 done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import io from "socket.io-client";
 function App() {
+  var socket = io.connect("http://localhost:8000");
+
+  const handleClick = () => {
+    emitData("super!!");
+  };
+  useEffect(() => {
+    socket.on("chat", (data) => {
+      setAns(data.message);
+    });
+  }, [socket]);
+
+  const [ans, setAns] = useState("no ans");
+
+  const emitData = (message) => {
+    socket.emit("chat", {
+      message: message,
+    });
+  };
+
   return (
     <div className="App">
       <h1>Hello CHAT</h1>
+      {ans}
+      <button onClick={handleClick}>Click me</button>
     </div>
   );
 }
