a668aa90a2ee2ac987e5448fe7fbc66bb2523aad
rishabh verma <vermarishabh0987@gmail.com>
2020-07-13 22:12:46 +0530

    resatrting fresh

--- src/App.js
+++ src/App.js
@@ -1,34 +1,14 @@
-import React, { useEffect, useState } from "react";
-import io from "socket.io-client";
+import React from "react";
+// import io from "socket.io-client";
 import Chat from "./chat/chat";
 import Process from "./process/process";
 import "./App.scss";
+
 function App() {
-  var socket = io.connect("http://localhost:8000");
-
-  const handleClick = ({ message, name }) => {
-    socket.emit("chat", {
-      message,
-      name,
-    });
-  };
-  useEffect(() => {
-    socket.on("chat", (data) => {
-      setAns(data);
-      setLeft(!left);
-    });
-  }, [socket]);
-
-  useEffect(() => {
-    alert("WORK UNDER CONSTRUCTION !! , THIS DOES NOT REPRESENT FINAL PRODUCT");
-  }, []);
-
-  const [ans, setAns] = useState(null);
-  const [left, setLeft] = useState(true);
   return (
     <div className="App">
       <div className="right">
-        <Chat handleClick={handleClick} data={ans} left={left} />
+        <Chat />
       </div>
       <div className="left">
         <Process />
--- src/chat/chat.js
+++ src/chat/chat.js
@@ -1,47 +1,32 @@
 import React, { useState } from "react";
 import "./chat.scss";
-function Chat({ handleClick, data, left }) {
-  const [message, setMessage] = useState("");
-  const [name, setName] = useState("");
+function Chat() {
+  const [text, setText] = useState("");
 
-  const sendData = () => {
-    handleClick({
-      message,
-      name,
-    });
-  };
+  const sendData = () => {};
 
-  console.log("data=", data);
   return (
     <div className="chat">
       <div className="user-name">
         <h2>Chat Now</h2>
       </div>
       <div className="chat-message">
-        {left ? (
-          <div className="message">
-            <p>{data?.message}</p>
-            <span>{data?.name}</span>
-          </div>
-        ) : (
-          <div className="message mess-right">
-            <p>{data?.message} </p>
-            <span>{data?.name}</span>
-          </div>
-        )}
-      </div>
-      <div className="send name">
-        <input
-          placeholder="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        ></input>
+        <div className="message">
+          <p>
+            Qui aliqua sunt nisi id laborum amet veniam enim eiusmod consequat.
+          </p>
+          <span>Rishabh</span>
+        </div>
+        <div className="message mess-right">
+          <p>something is not right </p>
+          <span>sanchit</span>
+        </div>
       </div>
       <div className="send">
         <input
           placeholder="enter your message"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
+          value={text}
+          onChange={(e) => setText(e.target.value)}
         ></input>
         <button onClick={sendData}>Send</button>
       </div>
